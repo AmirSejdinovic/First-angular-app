59eb35c3d853b1dbf69a4cdd6d9c337b453af3c0
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-20 13:02:06 +0100

    Propraty binidng

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,15 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   //selector: '[app-servers]',
   selector: 'app-servers',
-  template: `
+  /*template: `
   <app-server></app-server>
 
-  <app-server></app-server>`,
+  <app-server></app-server>`,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  //cerating proprti for bindin propraty
+    allowNewServer = false;
 
-  constructor() { }
+  constructor() { 
+    //Creating setTimeout method with es6 funciton declaration
+    setTimeout(() =>{
+      this.allowNewServer = true;
+    },2000);
+
+  }
 
   ngOnInit() {
   }
