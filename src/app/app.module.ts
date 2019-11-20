701f2a54d903c3c38903def9f52b62f100f4ec86
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-20 09:34:31 +0100

    Importing the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+//Importing the created ServerComponent from the relative path
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent//Adding the ServerComponent in the NgModule class, with this angular knows that we have ServerComponent and it will render it
   ],
   imports: [
     BrowserModule
