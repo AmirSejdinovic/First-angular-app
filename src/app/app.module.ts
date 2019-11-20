14e88ed1e1939906d9fad6cb9ca732ce38375f48
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-20 10:01:21 +0100

    Creating angular component wiht CLI
    Here I created the angular component through the terminal with comand ng generate component compenentname or ng g c comonennt name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 //Importing the created ServerComponent from the relative path
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    
   ],
   imports: [
     BrowserModule
