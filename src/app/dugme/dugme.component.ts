eac959253df434bf5efb4c8ec4c86c7c3fc1b8c2
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-21 13:38:05 +0100

    Fixing things

--- src/app/dugme/dugme.component.ts
+++ src/app/dugme/dugme.component.ts
@@ -17,6 +17,7 @@ export class Dugme{
 
      onToggleDisplay(){
        this.showSecret = !this.showSecret;
-       this.log.push(this.log.length + 1);
+       //this.log.push(this.log.length + 1);
+       this.log.push(new Date());
      }
 }
