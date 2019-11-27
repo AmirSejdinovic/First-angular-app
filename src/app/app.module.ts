dfa79ec5e1e480cc83a5e2a9209599a1081dbca5
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-27 14:57:31 +0100

    Components and databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-//Importing the created ServerComponent from the relative path
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { Warning } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
-import { ExcersiceApp } from './input/input.component';
-import { Dugme } from './dugme/dugme.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    Warning,
-    SuccessComponent,
-    ExcersiceApp,
-    Dugme
-    
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  
-
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
