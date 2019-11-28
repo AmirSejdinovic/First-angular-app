307b668e85757718c2ac744991dac7bd3d2ace4d
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-28 13:01:53 +0100

    Ngapp lifecycle

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,5 +1,18 @@
 //We must inport the input decorator from angular  core
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component,
+  OnInit, 
+  Input, 
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked
+ } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,13 +21,38 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   //This propraty is to enable or disable Angular to setup shadowDom etc the emulated propreties in the html element. This propraty we need to import from ANgular core. The default proprati is emualted etc turn on. if we do not want to see the strange propraties in the html element we can switch it to none
   encapsulation: ViewEncapsulation.Emulated //None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked {
   //@input is the Angular decorator which allow us to export the property to the public, and we than can call that propraty in another module
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string; 
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called');
+
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+     console.log('NgOnCanges called');
+     console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('NgIniti called');
+  }
+  ngDoCheck(){
+    console.log("ngDoCheck called");
+  }
+  ngAfterContentInit(){
+    console.log('ngAfter init called');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfrer connte cehced');
+  }
+  ngAfterViewInit(){
+    console.log('ng after w');
+  }
+  ngAfterViewChecked(){
+    console.log('ng after wev chekded');
   }
 
 }
