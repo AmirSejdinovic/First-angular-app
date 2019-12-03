d0397e4e134739bef265857c4115b2b4e1df67f3
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-03 11:44:45 +0100

    @HostBinding

--- src/app/better-higlight/better-higlight.directive.ts
+++ src/app/better-higlight/better-higlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBeterHal]'
 })
 export class BeterHal implements OnInit {
+  //Using the @HostBinding directive and declakring the proprety with the type of string and adding initial value. In the directive argument we pased the prorpaty which wee overide
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit(){
@@ -11,9 +13,11 @@ export class BeterHal implements OnInit {
   }
   //Ading hostListener directive for creating the dinamic hover
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
