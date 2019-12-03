f732c0551ffffdbf9c3a23cef6e4cd7e0b342f49
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-03 11:28:37 +0100

    @HostListener

--- src/app/better-higlight/better-higlight.directive.ts
+++ src/app/better-higlight/better-higlight.directive.ts
@@ -1,11 +1,19 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBeterHal]'
 })
 export class BeterHal implements OnInit {
+  
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit(){
+   // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+  //Ading hostListener directive for creating the dinamic hover
+  @HostListener('mouseenter') mouseover(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
