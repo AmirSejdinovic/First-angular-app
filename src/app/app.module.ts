9de1e59c94cb21c71b6f489fc2333528fa48cea3
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-04 12:56:05 +0100

    Creating and implementing services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-//Importing from file the directive class name
-import { BasicHeighDirective } from './basic-heighlight/basic-heiglight.directive';
 
 
 import { AppComponent } from './app.component';
-import {BeterHal} from './better-higlight/better-higlight.directive';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
-
-
-//Importing the custom directice
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHeighDirective,
-    BeterHal
-   
-   
-    
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule,
