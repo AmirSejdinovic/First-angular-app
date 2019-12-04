c4a62dc2b5e417dff635a43b832c33ef63b21b20
Amir Sejdinovic <amir24V@yahoo.com>
2019-12-04 14:20:43 +0100

    Creating services and implementing it

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,25 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccauntService } from '../account.service';
 
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccauntService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+ 
 
-  constructor(private logginService: LoggingService){
-
+  constructor(private logginService: LoggingService, private acountSerice: AccauntService){
+        
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.acountSerice.addAccount(accountName, accountStatus);
 
     this.logginService.logStatusChange(accountStatus);
     
